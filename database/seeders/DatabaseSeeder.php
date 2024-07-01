e082381fda2af7692964b206da63cf6cfd1b4704
sureshzz <sureshthapamagar8914@gmail.com>
2024-07-01 22:12:48 +0545

    initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Note;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'id'=> 1,
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('password123')
+        ]);
+        Note::factory(10)->create();
+    }
+}
